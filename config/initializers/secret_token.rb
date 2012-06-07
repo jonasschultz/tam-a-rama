4c28b6c509f05f700f269510621871b0ab46ba23
jonas <jonas@deployer.(none)>
2012-06-07 02:27:48 -0700

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TamARama::Application.config.secret_token = '40a1a448b0dedf23377924529d266726402b5e36883b5f0913baf7bfb9a1a3668e755464c7f04d124394530fe39656f563e9683965e1735fa2aa97f7ed1b6f41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TamARama::Application.config.session_store :cookie_store, :key => '_tam-a-rama_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TamARama::Application.config.session_store :active_record_store
